2078bd123bd29b8c938db6b7c8b0c815f6a0ce21
Nityam Madaan <130224087+w-nityammm@users.noreply.github.com>
2024-10-15 00:48:39 +0530

    updated for python3 and rendered output is now saved to an xml file

--- image2excel.py
+++ image2excel.py
@@ -1,24 +1,35 @@
 import os, sys
-import Image
+from PIL import Image
 from jinja2 import Template
-with file('xlsxart.template') as f:
-     source = f.read()
+
+with open('xlsxart.template') as f:
+    source = f.read()
+
 XimgTemplate = Template(source)
 im = Image.open("sample.png")
-mx,my = im.size
+mx, my = im.size
 pix = im.load()
+
 def RGBToHTMLColor(rgb_tuple):
     """ convert an (R, G, B) tuple to #RRGGBB """
     hexcolor = '#%02x%02x%02x' % rgb_tuple
     return hexcolor
+
 rows = []
 styles = {}
-for y in xrange(my):
+
+for y in range(my):
     row = []
-    for x in xrange(mx):
-        color = RGBToHTMLColor(pix[x,y])
-        styles[color] = {'name':color,'color':color}
-        cell = {'style_name':color}
+    for x in range(mx):
+        color = RGBToHTMLColor(pix[x, y])
+        styles[color] = {'name': color, 'color': color}
+        cell = {'style_name': color}
         row.append(cell)
     rows.append(row)
-print XimgTemplate.render(color_styles=styles.values(),rows=rows, imagesize={'x':mx,'y':my})
+
+output = XimgTemplate.render(color_styles=styles.values(), rows=rows, imagesize={'x': mx, 'y': my})
+
+with open('output.xml', 'w') as f:
+    f.write(output)
+
+print("Output saved to output.xml")
